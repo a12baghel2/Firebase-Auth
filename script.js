9c5ee0cb11fb18ba4c83cd860a2453ef1757cef3
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2021-06-01 18:43:28 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
 const db = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 function SignUp(){
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const name = document.getElementById('name').value;
     firebase.auth()
       .createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         // Signed in
-        var user = userCredential.user;
+        const user = userCredential.user;
         console.log(user.uid);
-        var firebaseRef = db.collection("users");
         const userData = {
+          name : name,
           email : email,
           firstLogin : true
         };
@@ -19,8 +21,6 @@ function SignUp(){
         setTimeout(function () {
           if(userData.firstLogin) {
             window.location.replace("/captureData.html");
-          }else{
-            window.location.replace("/Profile.html")
           }
         }, 1000);
       })
@@ -30,6 +30,7 @@ function SignUp(){
         window.alert(errorMessage)
       });
 }
+
 
 function Login(){
   const email = document.getElementById("email").value;
@@ -53,29 +54,44 @@ function Login(){
     });
 }
 
-function GoogleSignIn(){
-  var provider = new firebase.auth.GoogleAuthProvider();
+function GoogleSignIn() {
   firebase
     .auth()
     .signInWithPopup(provider)
     .then(function (result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-      var firebaseRef = db.collection("users");
+      window.location.replace("/Profile.html");
+    })
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(error.code);
+      console.log(error.message);
+    });
+}
+
+function GoogleSignUp(){
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      const token = result.credential.accessToken;
+      const user = result.user;
+      const firebaseRef = db.collection("users");
       const userData = {
         name: user.displayName,
         email: user.email,
-        firstLogin: true
+        firstLogin: true,
       };
       firebaseRef.doc(user.uid).set(userData);
       console.log(token);
       console.log(user);
       setTimeout(function () {
-        if(userData.firstLogin) {
-            window.location.replace("/captureData.html");
-          }else{
-            window.location.replace("/Profile.html");
-          }
+        if (userData.firstLogin) {
+          window.location.replace("/captureData.html");
+        } else {
+          window.location.replace("/Profile.html");
+        }
       }, 1000);
     })
     .catch(function (error) {
@@ -101,39 +117,22 @@ function LogOut() {
     });
 }
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    console.log(user.uid);
-    const docRef = db.collection("users").doc(user.uid);
-    docRef
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          const data = doc.data();
-          document.getElementById('userName').innerHTML = data.name;
-          document.getElementById('userEmail').innerHTML = data.email;
-        } else {
-          console.log("No such document!");
-        }
-      })
-      .catch((error) => {
-        console.log("Error getting document:", error);
-      });
-  } else {
-    // No user is signed in.
-  }
-});
+
 
 function Save(){
-  const name = document.getElementById('username').value;
+  const dateOfBirth = document.getElementById('dob').value;
   const bio = document.getElementById('bio').value;
   const birthplace = document.getElementById("birthplace").value;
+  const phoneNo = document.getElementById('phone').value;
+  const website = document.getElementById('website').value;
   const user = firebase.auth().currentUser;
   var firebaseRef = db.collection("users");
   const userData = {
-    name: name,
     bio : bio,
     birthplace : birthplace,
+    dateOfBirth: dateOfBirth,
+    phoneNo: phoneNo,
+    website: website
   };
   firebaseRef.doc(user.uid).set(userData, {merge : true});
   firebaseRef.doc(user.uid).update({firstLogin: false});
