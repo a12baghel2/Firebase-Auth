d34f75f129ea812d75484100e04dedae6c4ceb1b
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2021-06-01 11:03:28 +0530

    Created login and Profile

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//const db = firebase.firestore();
+
 function SignUp(){
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -7,10 +10,79 @@ function SignUp(){
         // Signed in
         var user = userCredential.user;
         console.log(user.uid);
+        setTimeout(function () {
+          window.location.replace("/login.html");
+        }, 1000);
       })
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
         window.alert(errorMessage)
       });
+}
+
+function Login(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log(user.uid);
+      const firebaseref = firebase.database().ref();
+      const userData = {
+        username : "DemoName",
+        userEmail : email,
+        userPassword : password
+      }
+      firebaseref.child(user.uid).set(userData)
+      setTimeout(function () {
+        window.location.replace("/Profile.html");
+      }, 1000);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert(errorMessage);
+    });
+}
+
+function GoogleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+
+      console.log(token);
+      console.log(user.displayName);
+      setTimeout(function () {
+        window.location.replace("/Profile.html");
+      }, 1000);
+    })
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(error.code);
+      console.log(error.message);
+    });
+}
+
+function LogOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      setTimeout(function () {
+        window.location.replace("/login.html");
+      }, 1000);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
